fd401cd04d9a6be55f9ab040f013fc22722143ec
Pablosky5xd <pm9463776@gmail.com>
2024-11-12 16:48:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-// Script para interactividad
-document.getElementById("contactButton").addEventListener("click", function() {
-    alert("¡Gracias por contactarnos! Pronto nos pondremos en contacto contigo.");
-});
+
